af1d93cf8a18b5ff9a4fcf2ef1524d838315c666
Marc Kohaupt <debuglevel@gmail.com>
2020-09-24 22:11:46 +0200

    Add TODOs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 import brother_ql
 
-app = FastAPI()
+class Print(BaseModel):
+    image_url: str
+    description: Optional[str] = None
 
+app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# TODO: check if there's something better provided
+@app.get("/health")
+async def get_health():
+    return {"status": "up"}
 
 # list all models supported by brother_ql
 @app.get("/models/")
@@ -21,6 +32,7 @@ async def list_labels():
     return brother_ql.devicedependent.label_type_specs
 
 # list all defined printers
+# TODO: no idea if we should do that or rely on a printer URL/label/etc defined in the prints request
 @app.get("/printers/")
 async def list_printers():
     pass
@@ -28,15 +40,26 @@ async def list_printers():
 # list all created prints
 @app.get("/prints/")
 async def list_prints():
+    # TODO: list all prints ever made
     pass
 
 # get a print
 @app.get("/prints/{item_id}")
 async def get_print(item_id: int):
+    # TODO: get data about a print
     pass
 
 # add a printing job
+# TODO: define proper HTTP status code
 @app.post("/prints/")
-async def post_prints():
-    pass
+async def post_prints(print: Print):
+    # TODO: check passed parameters (e.g. valid printer model and valid label type)
+    # TODO: download image_url to temporary file
+    # TODO: check if it's a PNG (maybe do not support SVG because the CairoSVG is just insane in it's size; although it would be nice as a SVG could be rasterized exactly for a label; alternative would be a rather closely coupled request to the other microservice which provides the PNG to provide a perfect sized PNG)
+    # TODO: maybe check the PNG size to report back whether resizing was needed
+    # TODO: save print data to a dictionary or an actual database
+    # TODO: send PNG to printer via brother_ql
+    # TODO: report back some data (id, size, needed resize, original data like label, model, etc). No idea if we should block. Maybe add a "blocking" attribute to JSON to choose that.
+    return print
+    #pass
 

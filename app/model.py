b42c0736386e4b34a3a97c28e18bd51d56263096
Marc Kohaupt <debuglevel@gmail.com>
2020-09-26 22:59:08 +0200

    Validate models

--- app/model.py
+++ app/model.py
@@ -6,3 +6,6 @@ logger = logging.getLogger(__name__)
 def get_all():
     # TODO: use brother_ql.models instead of deprecated brother_ql.devicedependent
     return brother_ql.devicedependent.models
+
+def is_valid(model: str):
+    return model in get_all()
--- tests/model_test.py
+++ tests/model_test.py
@@ -9,3 +9,12 @@ async def test_list_models():
     models = app.model.get_all()
     assert "QL-500" in models
     assert "QL-820NWB" in models
+
+@pytest.mark.asyncio
+async def test_is_valid():
+    """
+    Tests that a model is validated
+    """
+    assert app.model.is_valid("QL-500") == True
+    assert app.model.is_valid("QL-820NWB") == True
+    assert app.model.is_valid("invalid") == False

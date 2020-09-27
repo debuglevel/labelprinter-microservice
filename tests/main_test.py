c2910f2adfca03fb339e3da5b299dc7c6a83c2fb
Marc Kohaupt <debuglevel@gmail.com>
2020-09-27 14:53:38 +0200

    Fix tests for invalid models and labels

--- tests/main_test.py
+++ tests/main_test.py
@@ -24,22 +24,32 @@ def test_list_labels():
     assert response.status_code == 200
     assert "62" in response.json()
 
+def get_valid_print_json():
+    return {
+        "image_url": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Resistor_symbol_IEC.svg",
+        "red": False,
+        "low_quality": False,
+        "high_dpi": False,
+        "compress": False,
+        "printer_url": "tcp://127.0.0.1:9100",
+        "printer_backend": "network",
+        "printer_model": "QL-500",
+        "label_type": "62",
+        "description": "Whatever"
+    }
+
 def test_post_prints_with_invalid_model():
-    response = client.post(
-        "/prints/",
-        json = {
-            "model": "INVALID_MODEL"
-        }
-        )
+    json = get_valid_print_json()
+    json["printer_model"] = "INVALID_MODEL"
+
+    response = client.post("/prints/", json = json)
     assert response.status_code == 400
     assert "INVALID_MODEL" in response.text
 
 def test_post_prints_with_invalid_label():
-    response = client.post(
-        "/prints/",
-        json = {
-            "label": "INVALID_LABEL"
-        }
-        )
+    json = get_valid_print_json()
+    json["label_type"] = "INVALID_LABEL"
+
+    response = client.post("/prints/", json = json)
     assert response.status_code == 400
     assert "INVALID_LABEL" in response.text

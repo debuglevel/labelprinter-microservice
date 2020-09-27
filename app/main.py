bbf833ad52278a547cd5197b7b97b714c6163120
Marc Kohaupt <debuglevel@gmail.com>
2020-09-27 22:17:42 +0200

    Rename to AddPrintRequest

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-class PrintRequest(BaseModel):
+class AddPrintRequest(BaseModel):
     image_url: str
     description: Optional[str] = None
     red: bool
@@ -60,39 +60,39 @@ async def list_labels():
 
 
 @fastapi.post("/prints/")
-async def post_prints(print: PrintRequest):
+async def post_prints(addPrintRequest: AddPrintRequest):
     """
     Add a printing job
     """
     logger.info("Got POST request on /prints/")
 
     # check if given printer model and label type are valid
-    if app.model.is_valid(print.printer_model) == False:
+    if app.model.is_valid(addPrintRequest.printer_model) == False:
         raise HTTPException(
             status_code=400,
-            detail=f"printer model '{print.printer_model}' is invalid")
-    if app.label.is_valid(print.label_type) == False:
+            detail=f"printer model '{addPrintRequest.printer_model}' is invalid")
+    if app.label.is_valid(addPrintRequest.label_type) == False:
         raise HTTPException(
             status_code=400,
-            detail=f"label type '{print.label_type}' is invalid")
+            detail=f"label type '{addPrintRequest.label_type}' is invalid")
 
     # download image to temporary file
-    image_path, image_mimetype = app.image.download_image(print.image_url)
+    image_path, image_mimetype = app.image.download_image(addPrintRequest.image_url)
 
     # prepare image to be sent to a label printer (TODO: maybe that would be better placed in print_image() itself)
-    label_width = app.label.get_width(print.label_type)
+    label_width = app.label.get_width(addPrintRequest.label_type)
     prepared_image_path, is_resized = app.image.prepare_image(image_path, image_mimetype,
                                                   label_width)
 
     # send image to printer
     status = app.print.print_image(image_path=prepared_image_path,
-                                   printer_model=print.printer_model,
-                                   label_type=print.label_type,
-                                   printer_backend=print.printer_backend,
-                                   printer_url=print.printer_url,
-                                   red=print.red,
-                                   low_quality=print.low_quality,
-                                   high_dpi=print.high_dpi,
-                                   compress=print.compress)
+                                   printer_model=addPrintRequest.printer_model,
+                                   label_type=addPrintRequest.label_type,
+                                   printer_backend=addPrintRequest.printer_backend,
+                                   printer_url=addPrintRequest.printer_url,
+                                   red=addPrintRequest.red,
+                                   low_quality=addPrintRequest.low_quality,
+                                   high_dpi=addPrintRequest.high_dpi,
+                                   compress=addPrintRequest.compress)
     # TODO: report back some data (size, needed resize, original size, new size, original data like label, model, etc). No idea if we should block. Maybe add a "blocking" attribute to JSON to choose that.
-    return print
+    return addPrintRequest

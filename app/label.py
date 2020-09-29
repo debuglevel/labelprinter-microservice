0b44478cce363c655a8b48424c0b84cfa5cd3546
Marc Kohaupt <debuglevel@gmail.com>
2020-09-30 00:26:09 +0200

    Don't use devicedependent anymore but a wrapper instead

--- app/label.py
+++ app/label.py
@@ -3,6 +3,30 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def get_label_legacy_structures():
+    """
+    This is a slim variant of the legacy brother_ql.devicedependent interface: https://github.com/pklaus/brother_ql/blob/638b365d45421696e1c2cd26952093d68f87155b/brother_ql/devicedependent.py
+    It only exists to ensure compatibility if brother_ql.devicedependent is removed.
+    Callers should be adapted to the new interface.
+    """
+    label_type_specs = {}
+
+    from brother_ql.labels import LabelsManager
+    lm = LabelsManager()
+    for label in lm.iter_elements():
+        l = {}
+        l['name'] = label.name
+        l['kind'] = label.form_factor
+        l['color'] = label.color
+        l['tape_size'] = label.tape_size
+        l['dots_total'] = label.dots_total
+        l['dots_printable'] = label.dots_printable
+        l['right_margin_dots'] = label.offset_r
+        l['feed_margin'] = label.feed_margin
+        l['restrict_printers'] = label.restricted_to_models
+        label_type_specs[label.identifier] = l
+
+    return label_type_specs
 
 def get_width(label: str):
     """
@@ -10,7 +34,7 @@ def get_width(label: str):
     """
     logger.debug(f"Getting image width for '{label}' labels...")
 
-    labels = brother_ql.devicedependent.label_type_specs
+    labels = get_label_legacy_structures()
     width = labels[label]["dots_printable"][0]
 
     logger.debug(f"Got image width for '{label}' labels: {width}")
@@ -20,7 +44,7 @@ def get_width(label: str):
 def get_all():
     logger.debug(f"Getting all label types...")
     # TODO: use brother_ql.labels instead of deprecated brother_ql.devicedependent
-    labels = brother_ql.devicedependent.label_type_specs
+    labels = get_label_legacy_structures()
     logger.debug(f"Got all label types")
     return labels
 
--- app/model.py
+++ app/model.py
@@ -3,11 +3,24 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def get_model_legacy_structures():
+    """
+    This is a slim variant of the legacy brother_ql.devicedependent interface: https://github.com/pklaus/brother_ql/blob/638b365d45421696e1c2cd26952093d68f87155b/brother_ql/devicedependent.py
+    It only exists to ensure compatibility if brother_ql.devicedependent is removed.
+    Callers should be adapted to the new interface.
+    """
+    from brother_ql.models import ModelsManager
+    models = []
+
+    for model in ModelsManager().iter_elements():
+        models.append(model.identifier)
+
+    return models
 
 def get_all():
     logger.debug(f"Getting all printer models...")
     # TODO: use brother_ql.models instead of deprecated brother_ql.devicedependent
-    models = brother_ql.devicedependent.models
+    models = get_model_legacy_structures()
     logger.debug(f"Got all printer models")
     return models
 
